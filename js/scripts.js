654899461b09efad173c1750bb339967419927f3
JaredRamirez <jaredelboca@gmail.com>
2025-02-01 13:21:05 -0800

    site for jikas auto

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(window).on('scroll', function() {
+  if ($(this).scrollTop() > 150) {
+      $('.navbar').addClass('scrolled');
+  } else {
+      $('.navbar').removeClass('scrolled');
+  }
+});
+
+$(document).ready(function() {
+ 
+
+
+}); //doc ready
+
